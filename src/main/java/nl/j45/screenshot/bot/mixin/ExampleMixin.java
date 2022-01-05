cd294133d5919a31e47b93cb3bc9b66a7d6919ae
Jeroen Tas <jeroentas@live.nl>
2022-01-05 12:38:39 +0100

    Initial commit

---
+++ src/main/java/nl/j45/screenshot/bot/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package nl.j45.screenshot.bot.mixin;
+
+import nl.j45.screenshot.bot.ScreenshotBot;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		ScreenshotBot.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
